8de432346d3381ea9bf344509718324b47537e1c
Don Welch <dwelch@nait.ca>
2026-01-22 19:48:43 -0700

    introduce classes and coding of a class

---
+++ OOPsProgramming/LearningObjects/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//a namespace is use to indicate organization with our code
+//all items within the same namespace can be referenced without
+//  addition using clauses
+//default namespaces are the project name
+
+namespace LearningObjects
+{
+    //the access level of your class definition
+    //public
+    //  can be use by any code that has access to the namespace
+    //private
+    //  ?????
+    //internal 
+    //  can be use by any other code within the same structure
+    //      as the class definition
+
+    //a class is considered a valid C# datatype
+    //this datatype is a programmer defined datatype
+    public class Person
+    {
+        //data member
+
+        //fields, attributes, data members (data characteristics)
+        //hold a piece of data
+        //data is valuable
+        //securing access by making them private
+        //access and modification will be done via
+        // other components of the class
+        private string Name;
+        private int Age;
+        private decimal Wage;
+
+        //properties
+
+
+
+        //methods
+        //  constructor
+
+
+        // behaviour (aka method)
+    }
+}
---
+++ OOPsProgramming/LearningObjects/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LearningObjects;
+
+Console.WriteLine("Hello, World!");
+
+//Person is a valid C# datatype
+Person me;
