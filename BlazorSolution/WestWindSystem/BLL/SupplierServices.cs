23580f2284fb669e761b30e3c2e14e88efd5f4bc
Don Welch <dwelch@nait.ca>
2026-03-24 19:53:08 -0600

    RE entites of WestWind, add CRUD demo UI pages and library service classes

---
+++ BlazorSolution/WestWindSystem/BLL/SupplierServices.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+#region Additional Namespaces
+using WestWindSystem.DAL;
+using WestWindSystem.Models;
+#endregion
+
+namespace WestWindSystem.BLL
+{
+    public class SupplierServices
+    {
+        #region setup constructor and private local context variable
+        private readonly WestWindContext _context;
+
+        public SupplierServices(WestWindContext context)
+        {
+            _context = context;
+        }
+        #endregion
+
+        #region Queries
+        public async Task<List<Supplier>> Supplier_GetAll()
+        {
+            return await _context.Suppliers.ToListAsync();
+        }
+        #endregion
+    }
+}
